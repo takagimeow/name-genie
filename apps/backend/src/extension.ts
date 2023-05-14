d33e9e5c89587f6b205ff6fa1666309291225828
Keisuke Takagi <mit@takagi.app>
2023-05-14 20:04:58 +0900

    fix: remove registerCommand call
    registerCommandの呼び出しを削除しました

--- apps/backend/src/extension.ts
+++ apps/backend/src/extension.ts
@@ -14,21 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "name-genie" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('name-genie.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Name Genie!');
-	});
-
 	let disposableWebView = vscode.window.registerWebviewViewProvider(
 		"name-genie.webview",
 		new WebViewProvider(context.extensionUri, context.extensionPath)
 	);
 
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableWebView);
 }
 
